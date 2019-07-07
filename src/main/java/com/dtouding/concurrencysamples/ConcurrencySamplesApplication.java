a2f58fecf8da4b6ad073bc1be8d8e48846969ed1
gujianzhe <1258993213@qq.com>
2019-07-07 14:32:09 +0800

    'init'

---
+++ src/main/java/com/dtouding/concurrencysamples/ConcurrencySamplesApplication.java
@@ -0,0 +1,13 @@
+package com.dtouding.concurrencysamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencySamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencySamplesApplication.class, args);
+    }
+
+}
